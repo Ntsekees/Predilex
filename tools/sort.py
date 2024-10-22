360a4bdeb1cf3009b7bff12ddffcc13491b4718c
Ntsekees <ntsekees@users.noreply.github.com>
2024-10-22 15:03:23 +0200

    Abandoning the “ordering” and “subordering” columns, transferring their function to the ⟦tools/sort.py⟧ script instead. Removal of the “serialization” column, addition of columns for Xextan and Nahaıwa lemmas. Some change in Predilex entries.

--- tools/sort.py
+++ tools/sort.py
@@ -15,25 +15,38 @@ SELF_PATH = os.path.dirname(os.path.realpath(__file__))
 # ============================================================ #
 
 def entrypoint():
-  start_time = time.time()
-  edit_csv_from_path(
-    SELF_PATH + "/../predilex.csv",
-    predilex_sorted_from,
-    output_path = SELF_PATH + "/../predilex.csv"
-  )
-  print("Execution time: {:.3f}s.".format(
-    time.time() - start_time))
+	start_time = time.time()
+	edit_csv_from_path(
+		SELF_PATH + "/../predilex.csv",
+		predilex_sorted_from,
+		output_path = SELF_PATH + "/../predilex.csv"
+	)
+	print("Execution time: {:.3f}s.".format(
+		time.time() - start_time))
 
 def predilex_sorted_from(predilex):
-  header, body = predilex[:2], predilex[2:]
-  sorting_cols = ("tags", "subordering", "ordering")
-  for c in sorting_cols:
-    ci = header[0].index(c)
-    body = sorted_by_col_id(body, ci)
-  return header + body
+	header, body = predilex[:2], predilex[2:]
+	for col in ("eng_def", "tags"):
+		body = sorted_by_col_id(body, header[0].index(col))
+	body = sorted(
+		body,
+		key = lambda row: tag_rank_of_row(row, header[0].index("tags")))
+	return header + body
 
-def sorted_by_col_id(table, ci):
-  return sorted(table, key = lambda t: (t[ci] == "", t[ci]))
+def sorted_by_col_id(table, cid):
+	return sorted(table, key = lambda t: (t[cid] == "", t[cid]))
+
+def tag_rank_of_row(row, tags_col_id):
+	tags = row[tags_col_id]
+	l = tags.split(" ")
+	first = "" if l == [] else l[0]
+	if first in HIERARCHY:
+		return HIERARCHY.index(first)
+	else:
+		return len(HIERARCHY)
+
+HIERARCHY_S = "constant, basic, mereology, context_handling, worlds, logic, mathematics, cardinality, ordinality, prop_cardinality, shape, spacetime, time, space, causality, philosophy, perception, has_color, mind, emotion, psychology, sociology, cognition, evidentiality, deixis, aspect, directional_part, position, spatiotemporal_position, temporal_position, spatial_position, physics, physical_property, action, involuntary_action, action, body_part, biology, substance, material, artifact, chemical_element, culture, cultural, amount, astronomy, food, beverage, change, chemistry, computer_language, computer_science, currency, duplicate, dwelling, ethny, game, geographic_entity, geographic_feature, geology, has_illness, illness, joke, kinship, landscape, language, linguistics, measure_unit, medical_condition, modality, natural_feature, natural_phenomenon, medicine, profession, programming_language, pseudo-absolute, quaternary_predicate, religion, room, science, season, semiotics, supernatural, syntax, taxonomy, weather, texture, thematic_role, toaq_copy, ⚠"
+HIERARCHY = HIERARCHY_S.split(", ")
 
 # ============================================================ #
 
